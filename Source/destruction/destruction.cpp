07f49146b6ff50e56f7abc8e375c8086e7541720
breakfastpasta <breakfastpasta@protonmail.com>
2026-02-28 18:17:46 -0700

    initial commit

---
+++ Source/destruction/destruction.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "destruction.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, destruction, "destruction" );
